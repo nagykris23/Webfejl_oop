88622000afbb0700748d326f0c144e6d61d5a734
Nagy Kristof <nagy.kristof@BOLYAI.LOCAL>
2024-11-29 15:30:59 +0100

    jobb fortizgatások a víktóri rolyál is meglett gomszabbal

--- basics/script.js
+++ basics/script.js
@@ -1,31 +1,56 @@
 
-function player(nickname) {
-    this.nickname = nickname;
-    this.playedMatch = 0;
+// function player(nickname) {
+//     this.nickname = nickname;
+//     this.playedMatch = 0;
+// }
+// player.prototype.play = function () {
+//     this.playedMatch++;
+//     console.log(this.nickname, this.playedMatch)
+// }
+
+// player.prototype.gettierlevel = function () {
+//     if (this.playedMatch < 4) {
+//         return "A"
+//     }
+//     else if ( this.playedMatch < 7 && this.playedMatch > 3) {
+//         return "B"
+//     }
+//     else {
+//         return "C"
+//     }
+
+// }
+
+class player {
+    constructor(nickname) {
+        this.nickname = nickname;
+        this.playedMatch = 0;
+    }
+    play() {
+        this.playedMatch++;
+        console.log(this.nickname, this.playedMatch)
+    }
+    gettierlevel() {
+        if (this.playedMatch < 4) {
+            return "A"
+        }
+        else if (this.playedMatch < 7 && this.playedMatch > 3) {
+            return "B"
+        }
+        else {
+            return "C"
+        }
+
+    }
 }
-player.prototype.play = function () {
-    this.playedMatch++;
-    console.log(this.nickname, this.playedMatch)
-}
+
+
 const gomszab = new player("gomszab1");
 
 console.log(gomszab)
 
 
-
-player.prototype.gettierlevel = function () {
-    if (this.playedMatch < 4) {
-        return "A"
-    }
-    else if ( this.playedMatch < 7 && this.playedMatch > 3) {
-        return "B"
-    }
-    else {
-        return "C"
-    }
-
-}
-console.log(gomszab.gettierlevel())
 gomszab.play();
+console.log(gomszab.gettierlevel())
 gomszab.play();
 console.log(gomszab.gettierlevel())
