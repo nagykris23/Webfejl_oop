9a10cbe90e664bb637042a3f5a7d31b7a866f4c4
Kristof <nagykris68@gmail.com>
2024-12-02 15:01:04 +0100

    pörög a forti tovább ch6 s1 megindult

--- basics/script.js
+++ basics/script.js
@@ -54,3 +54,18 @@ gomszab.play();
 console.log(gomszab.gettierlevel())
 gomszab.play();
 console.log(gomszab.gettierlevel())
+
+function Person(name){
+    this.name = name
+}
+
+Person.prototype.GetName = function(){
+    return this.name
+}
+
+function Student(name, school){
+    Person.call(this, name)
+    this.school = school
+}
+
+Object.setPrototypeOf(Student.prototype, Person.prototype)
