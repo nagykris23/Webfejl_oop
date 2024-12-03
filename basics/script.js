37cc56724fd258f7ab6ff9870bde8271f6c61eb4
Kristof <nagykris68@gmail.com>
2024-12-03 17:44:13 +0100

    Kommentezés meg van ha minden igaz

--- basics/script.js
+++ basics/script.js
@@ -21,99 +21,126 @@
 
 // }
 
+// Player osztály létrehozása
 class player {
-    constructor(nickname) {
+    constructor(nickname) { 
+        // Játékos becenevét tárolja
         this.nickname = nickname;
+        //meccsek száma (kezdetben 0)
         this.playedMatch = 0;
     }
+    
+    //  növeli a játszott meccsek számát és kiírja a játékos nevét + játszott meccsek számát
     play() {
         this.playedMatch++;
-        console.log(this.nickname, this.playedMatch)
+        console.log(this.nickname, this.playedMatch);
     }
-    gettierlevel() {
-        if (this.playedMatch < 4) {
-            return "A"
-        }
-        else if (this.playedMatch < 7 && this.playedMatch > 3) {
-            return "B"
-        }
-        else {
-            return "C"
-        }
 
+    // visszaadja a játékos szintjét a játszott mecsek száma alaoján
+    gettierlevel() {
+        if (this.playedMatch < 4) { 
+            return "A"; // ha a játszott meccsek száma kevesebb mint 4 akkor a tier
+        } else if (this.playedMatch < 7 && this.playedMatch > 3) {
+            return "B"; // ha a játszott meccsek 4 és 6 között vannak akkor b tier
+        } else {
+            return "C"; // ha a játszott meccsek száma legalább 7 vagy több akkor c tier
+        }
     }
 }
 
 
+// Új játékos létrehozása
 const gomszab = new player("gomszab1");
 
-console.log(gomszab)
+console.log(gomszab); // A játékos kiíratása
 
-
-gomszab.play();
-console.log(gomszab.gettierlevel())
-gomszab.play();
-console.log(gomszab.gettierlevel())
+gomszab.play(); // Meccs játszása
+console.log(gomszab.gettierlevel()); //A szint kiirása 
+gomszab.play(); // Meccs játszása
+console.log(gomszab.gettierlevel()); // Szint kiirása
 
 // function Person(name){
 //     this.name = name
 // }
+
+// Person osztály létrehozása
 class Person {
     constructor(name) {
-        this.name = name
+        this.name = name; // Személy nevét tárolja
     }
+
+    // visszaadja a személy nevét
     getName() {
-        return this.name
+        return this.name;
     }
 }
-Person.prototype.getName = function () {
-    return this.name
-}
+
+
+// Person.prototype.getName = function () {
+//     return this.name
+// }
 
 // function Student(name, school){
 //     Person.call(this, name)
 //     this.school = school
 // }
+
+// Student osztály, amely örökli a Person osztály tulajdonságait
 class Student extends Person {
     constructor(name, school) {
-        super(name)
-        this.school = school;
+        super(name); // Meghívja a Person osztályt
+        this.school = school; // Diák iskoláját tárolja
     }
 }
 
 // Object.setPrototypeOf(Student.prototype, Person.prototype)
 
-const student = new Student("Géza", "Bolyai")
-console.log("név " + student.getName()+"|"+ "iskola " + student.school)
+// Új diák létrehozása
+const student = new Student("Géza", "Bolyai");
+console.log("név " + student.getName() + " | iskola " + student.school); // Diák neve és iskolájának kiirása
 
+// Animal osztály létrehozása
 class Animal {
     constructor(name) {
-        this.name = name;
+        this.name = name; // Állat nevét tárolja
     }
+
+    // kiírja hogy az állat hangot ad ki
     hang() {
-        console.log(this.name + " hangot ad ki")
+        console.log(this.name + " hangot ad ki");
     }
 }
+// Bird osztály amely örökli az Animal osztály tulajdonságait
 class Bird extends Animal {
     constructor(name) {
-        super(name)
+        super(name); // Meghívja az Animal osztályt
     }
+
+    // kiírja hogy a madár repül
     repül() {
-        console.log(this.name + " repül")
+        console.log(this.name + " repül");
     }
 }
+
+// Mammal osztály amely örökli az Animal osztály tulajdonságait
 class Mammal extends Animal {
     constructor(name) {
-        super(name)
+        super(name); // Meghívja az Animal osztályt
     }
-    sétal(){
-        console.log(this.name + " gyalogol")
+
+    //kiírja, hogy az emlős gyalogol
+    sétal() {
+        console.log(this.name + " gyalogol");
     }
 }
-const bird = new Bird("cinege");
-bird.hang()
-bird.repül()
 
-const mammal = new Mammal("Macska")
-mammal.hang()
-mammal.sétal()
+//Új bird létrehozása
+const bird = new Bird("cinege");
+bird.hang(); // Madár hangot ad
+bird.repül(); // Madár repül
+
+// ugyan az mint a madár 
+// Új mammal létrehozása
+const mammal = new Mammal("Macska");
+mammal.hang(); // Emlős hangot ad
+mammal.sétal(); // Emlős gyalogol
