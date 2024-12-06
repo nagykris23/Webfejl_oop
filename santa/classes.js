03f0474a661a8e39d665ed840100915d250e3bc3
Nagy Kristof <nagy.kristof@BOLYAI.LOCAL>
2024-12-06 15:30:46 +0100

    bombasztikus haladás a 6os megvan talán

--- santa/classes.js
+++ santa/classes.js
@@ -1,8 +1,29 @@
 
-class Factory{
+class Factory {
     // TODO 1, 2, 3, 4, 9, 10
-   }
-   
-   class Companion{
+
+    constructor() {// nem kell tulajdonság mert késöbb fogjuk feltölteni
+        this.manolista = []//üres lista manolista példányosításakor
+    }
+    manoadd(mano) {//függvény letrehozasa mano parameter mert feltoltjuk
+        this.manolista.push(mano);//manolistahoz hozza fuzunk egy elemet this egy factory peldany
+    }
+}
+
+class Companion {
     // TODO 5
-   }
+    constructor(id, kernev, veznev, reszleg) {//ezek nelkul nem ertelmezzuk a peldat
+        this.id = id;
+        this.kernev = kernev;
+        this.veznev = veznev;
+        this.reszleg = reszleg;
+
+        this.productlist = [];//ures tomb nincs cucc mikor regel
+    }
+    getname(){//nem kell neki parameter mert a peldany parameteret latja 
+        return this.kernev + "|" + this.veznev;
+    }
+   addpruduct(product){
+        this.productlist.push(product);//ez is hozza adja a pruductot a listahoz
+   } 
+}
--- santa/main.js
+++ santa/main.js
@@ -8,33 +8,33 @@
 */
 const companionList = [
    {
-       firstName: 'Géza',
-       lastName: 'Kiss',
-       area: 'plastic',
-       products: ['kisauto', 'barbibaba']
+      firstName: 'Géza',
+      lastName: 'Kiss',
+      area: 'plastic',
+      products: ['kisauto', 'barbibaba']
    },
    {
-       firstName: 'Ferenc',
-       lastName: 'Tóth',
-       area: 'paper',
-       products: ['könyv']
+      firstName: 'Ferenc',
+      lastName: 'Tóth',
+      area: 'paper',
+      products: ['könyv']
    },
    {
-       firstName: 'Gábor',
-       lastName: 'Nagy',
-       area: 'plastic',
-       products: ['zokni']
+      firstName: 'Gábor',
+      lastName: 'Nagy',
+      area: 'plastic',
+      products: ['zokni']
    },
 ]
 const factory = new Factory();
 
-document.getElementById('companion').addEventListener('submit',function(e){
+document.getElementById('companion').addEventListener('submit', function (e) {
    e.preventDefault();
-   const form =  e.currentTarget
+   const form = e.currentTarget
    addCompanion(form, factory);
 });
 
-document.getElementById('product').addEventListener('submit',function(e){
+document.getElementById('product').addEventListener('submit', function (e) {
    e.preventDefault();
    const form = e.currentTarget;
    addProductForm(form, factory)
@@ -43,9 +43,24 @@ document.getElementById('product').addEventListener('submit',function(e){
 /**
 * table render
 */
-function initTable(){
-
+function initTable() {
    // TODO 6
+   for (let i = 0; i < companionList.length; i++) {// vegig megyünk a listan
+      const currentElement = companionList[i]//valtozoba bombazzuk a lista iedik elem
+      const companion = new Companion(//peldanyositjuk a compaiont a classbol a sorrend a calass construktora ertekei megadasatol fugg  
+         i,
+         currentElement.firstName,
+         currentElement.lastName,
+         currentElement.area
+      )
+      for(const pr of currentElement.products){// vegig megyunk a productson mert egyessevel adjuk hozza a productokat a compainhoz
+         companion.addpruduct(pr)//itt adjuk hozza 
+      }
+      factory.manoadd(companion)//hozzaadjuk a manot a factoryhoz(ugyan az mint a mano)
+   }
+   console.log(factory)//consolba logolas
+
+
 }
 
 
@@ -57,7 +72,7 @@ initTable()
 * 
 * @param {EventTarget} e 
 */
-function checkEventListener(e){
+function checkEventListener(e) {
    const row = e.currentTarget.parentElement.parentElement;
    const companionId = row.id;
    // TODO 10
