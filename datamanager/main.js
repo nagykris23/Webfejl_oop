446ccc621949b560654c34b907782c0e1f620f4f
Nagy Kristof <nagy.kristof@BOLYAI.LOCAL>
2025-01-31 10:05:55 +0100

    nem jó

---
+++ datamanager/main.js
@@ -0,0 +1,116 @@
+/**
+ * @typedef {{nev:String, eletkor:Number}} Person
+ * @callback UpdateCallback
+ * @param {Person[]} person
+ * @returns {void}
+ * 
+ * 
+ */
+class Datamanager {
+    /**
+    * @type {Person[]}
+    */
+    #array
+    /**
+     * @type {UpdateCallback}
+     */
+    #callback
+    /**
+     * 
+     * @param {Person[]} kiscica 
+     */
+    constructor(kiscica = [])
+    {
+        this.#array= kiscica
+        this.#callback = () =>{}
+    }
+    /**
+     * 
+     * @param {UpdateCallback} callback 
+     */
+    setUpdateCallback(callback){
+        this.#callback= callback
+        this.#callback(this.#array)
+    }
+/**
+ * 
+ * @param {Person[]} sok 
+ */
+    add(sok){
+        this.#array.push(sok)
+        this.#callback(this.#array)
+           
+    }
+    /**
+     * 
+     * @param {Number} kor 
+     */
+    filterage(kor){
+        const result = []
+        for(const elem of this.#array){
+            if(elem.eletkor === kor){
+                result.push(elem)
+            }
+        }
+        this.#callback(result)
+    }
+/**
+ * 
+ * @param {String} neve 
+ */
+    filtername(neve){
+        const result = []
+        for(const elem of this.#array){
+            if(elem.nev.includes(neve)){
+                result.push(elem)
+            }
+        }
+        this.#callback(result)
+    }
+}
+
+class Datatable {
+    /**
+     * 
+     * @param {Datamanager} datamanager 
+     */
+constructor(datamanager)
+{
+    const table = document.createElement('table')
+    document.body.appendChild(table)
+    
+    const header = document.createElement('thead')
+    table.appendChild(header)
+
+    const tbody = document.createElement('tbody')
+    table.appendChild(tbody)
+
+    datamanager.setUpdateCallback((persons)=>{
+        tbody.innerHTML=''
+        for(const pers of persons){
+            const sor =document.createElement('tr')
+            tbody.appendChild(sor)
+
+            const cella = document.createElement('td')
+            cella.innerHTML= pers.nev
+            sor.appendChild(cella)
+
+            const cella2 = document.createElement('td')
+            cella2.innerHTML= pers.eletkor
+            sor.appendChild(cella2)
+
+        }
+    })
+
+}
+}
+
+const datamanager = new Datamanager([{eletkor: 18, nev:"Levente"},{eletkor: 23, nev:"Gergő"},{eletkor: 11, nev:"Máté"}])
+const datatable = new Datatable(datamanager)
+
+const input = document.createElement('input')
+tbody.appendChild(input)
+input.addEventListener('input',(e)=>{
+    filterage(e.currentTarget.value)
+    filtername(e.currentTarget.value)
+})
